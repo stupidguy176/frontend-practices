ad37a7e75bba9bfbf678110cdb93e802d6dd8b6a
stupidguy176 <hoaian176@gmail.com>
2020-04-24 00:26:05 +0700

    init finish

--- [16]sugar-tets/js/main.js
+++ [16]sugar-tets/js/main.js
@@ -1,69 +1,188 @@
 "use strict";
 
-
-function Navigation() {
-    var bodyEl = document.body;
-
-    function init() {
-        initEvents();
-    }
-
-    init();
-};
-
+var my_list = [];
 
 window.onload = function () {
+  my_list = getOpenItems();
+  if (my_list) {
+    render();
+  }
+}
+
+function getOpenItems() {
+  let my_list = [
+    { name: "Movies to watch", openItems: ["Interstella", "DogCat"] },
+    { name: "Yooooo", openItems: [] }
+  ]
+  return my_list;
+}
+
+function addNewList() {
+  let name = document.getElementById('input_item').value;
+  if (name !== '') {
+    let item = {
+      name: name,
+      openItems: []
+    }
+    my_list.push(item);
+    renderNewList(item);
+  }
+  document.getElementById('input_item').value = null;
+}
+
+function render() {
+  document.getElementById('list-item-data').innerHTML = '';
+  my_list.forEach(item => {
+    let open_items = item.openItems;
+    let slug = convertToSlug(item.name);
+    let html_li = ``;
+    if (open_items.length > 0) {
+      open_items.forEach(item => {
+        let slug_item = convertToSlug(item);
+        let tag_li = `
+              <li id=li-${slug}-${slug_item}>
+                <input class="styled-checkbox" id=${slug}-${item} type="checkbox">
+                <label for=${slug}-${item}>${item}</label>
+                <span id="btn-delete" class="btn-delete" onclick='deleteLi(\"${slug}\",\"${item}\")'>&#10005</span>
+              </li>
+            `
+        html_li += tag_li;
+      })
+    }
+    const html = `
+            <div class="list-item">
+            <div class="title">
+              <div id="title-info-${slug}" class="title-info">
+                <span id="edit-title" onclick='editItem(\"${slug}\")'>${item.name}</span>
+                <div class="icons">
+                  <span id="add-item" class="icon-sm" onclick='addOpenItem(\"${item.name}\", \"${slug}\")'><img src="images/icon/add.svg" alt=""></span>
+                  <span id="delete-item" class="icon-sm" onclick='deleteItem(\"${item.name}\")'><img src="images/icon/delete.svg" alt=""></span>
+                </div>
+              </div>
+              <div id="title-add-${slug}" class="title-add display-none">
+                <input id="input-${slug}" type="text" placeholder="${item.name}">
+                <span class="icon-sm" onclick='saveItem(\"${item.name}\",\"${slug}\")'><img src="images/icon/save.svg" alt=""></span>
+              </div>
+            </div>
+            <div class="content">
+              <div id="ioitem-${slug}" class="add-new-item display-none">
+                <input type="text" placeholder="New item">
+              </div>
+              <ul id="${slug}">
+                ${html_li}
+              </ul>
+            </div>
+          </div>
+        `;
+    document.getElementById('list-item-data').innerHTML += html;
+  })
+}
+
+function renderNewList(item) {
+  let html_li = ``;
+  let slug = convertToSlug(item.name);
+  const html = `
+      <div class="list-item">
+      <div class="title">
+        <div id="title-info-${slug}" class="title-info">
+          <span id="edit-title" onclick='editItem(\"${slug}\")'>${item.name}</span>
+          <div class="icons">
+            <span id="add-item" class="icon-sm" onclick='addOpenItem(\"${item.name}\", \"${slug}\")'><img src="images/icon/add.svg" alt=""></span>
+            <span id="delete-item" class="icon-sm" onclick='deleteItem(\"${item.name}\")'><img src="images/icon/delete.svg" alt=""></span>
+          </div>
+        </div>
+        <div id="title-add-${slug}" class="title-add display-none">
+          <input id="input-${slug}" type="text" placeholder="${item.name}">
+          <span class="icon-sm" onclick='saveItem(\"${item.name}\", \"${slug}\")'><img src="images/icon/save.svg" alt=""></span>
+        </div>
+      </div>
+      <div class="content">
+        <div id="ioitem-${slug}" class="add-new-item display-none">
+          <input type="text" placeholder="New item">
+        </div>
+        <ul id="${slug}">
+          ${html_li}
+        </ul>
+      </div>
+    </div>
+  `;
+  document.getElementById('list-item-data').innerHTML += html;
+}
+
+function deleteItem(name) {
+  my_list = my_list.filter(i => i.name !== name);
+  render();
+}
+
+function editItem(slug) {
+  var title_info = document.getElementById(`title-info-${slug}`);
+  title_info.classList.add("display-none");
+  var el = document.getElementById(`title-add-${slug}`);
+  el.classList.remove("display-none");
+}
+
+function saveItem(name, slug) {
+  var title_info = document.getElementById(`title-info-${slug}`);
+  title_info.classList.remove("display-none");
+  var el = document.getElementById(`title-add-${slug}`);
+  el.classList.add("display-none");
+  var val = document.getElementById(`input-${slug}`).value;
+  my_list.forEach(i => {
+    if (i.name == name) {
+      i.name = val;
+    }
+  })
+
+  render();
 
 }
 
-function memeo(x, f = () => x) {
-    var x;
-    var y = x;
-    x = 2;
-    return [x, y, f()];
+function convertToSlug(Text) {
+  return Text
+    .toLowerCase()
+    .replace(/[^\w ]+/g, '')
+    .replace(/ +/g, '-')
+    ;
 }
-var myFunction = (a) => a;
 
-var leapYear = function (year) {
-    if ((year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
-        return true
-    } else {
-        return false
+function addOpenItem(name, slug) {
+  var io = document.getElementById(`ioitem-${slug}`);
+  io.classList.remove("display-none");
+  document.onkeyup = function (evt) {
+    evt = evt || window.event;
+    let c = document.activeElement;
+    let pr = c.parentNode;
+    if (evt.key == 'Escape') {
+      pr.classList.add("display-none");
     }
-};
-
-var leapYear = function (year) {
-    if (year % 4 === 0) {
-        if(year % 100 === 0) {
-            if(year % 400 === 0) {
-                return true;
-            }
-            return false;
+    if (evt.key == 'Enter') {
+      let id = pr.id.slice(7);
+      var ck = document.querySelector(`ul[id=${id}]`);
+      var elChild = document.createElement('li');
+      let vl = pr.firstElementChild.value;
+      let slug_item = convertToSlug(vl);
+      elChild.id = `li-${slug}-${slug_item}`;
+      let tag_li = `
+        <input class="styled-checkbox" id=${slug}-${vl} type="checkbox">
+        <label for=${slug}-${vl}>${vl}</label>
+        <span id="btn-delete" class="btn-delete" onclick='deleteLi(\"${slug}\",\"${vl}\")'>&#10005</span>
+      `;
+      elChild.innerHTML = tag_li;
+      if (!ck) { return; }
+      ck.appendChild(elChild);
+      my_list.forEach(i => {
+        if (i.name == name) {
+          i.openItems.push(vl);
+          i.openItems.sort();
         }
-        return true;
-    } else {
-        return false
+      })
+
     }
-};
+  }
+}
 
-
-function strReplace(my_strings){
-    const regex = /g/gi;
-    const regex1 = /o/gi;
-    const  regex2 = /e/gi;
-
-    let new_str = my_strings.replace(regex, 'A');
-
-    let new_str_3 = '';
-
-    if(new_str) {
-        let new_str_2 = new_str.replace(regex1, 'q');
-        if(new_str_2) {
-            new_str_3 = new_str_2.replace(regex2, 'g');
-            console.log('3a: ',new_str_3);
-        }
-    }
-
-    console.log('3b: ',new_str_3);
-    return new_str_3;
+function deleteLi(slug, vl) {
+  let slug_item = convertToSlug(vl);
+  var li_item = document.querySelector(`li[id=li-${slug}-${slug_item}]`);
+  li_item.remove();
 }
