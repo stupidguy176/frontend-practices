54285ae658724050e4ef52f66e08554717f1269f
stupidguy176 <hoaian176@gmail.com>
2020-04-24 14:41:55 +0700

    dragdrop

--- [16]sugar-tets/js/main.js
+++ [16]sugar-tets/js/main.js
@@ -2,6 +2,181 @@
 
 var my_list = [];
 
+var droppables = new Array();
+var itemBeingDragged = null;
+var mouseDownPoint = { x: 0, y: 0 };
+
+function onDocumentMouseMove(mouseMoveEvent) {
+  var point = { x: mouseMoveEvent.pageX, y: mouseMoveEvent.pageY };
+
+  // Drag the draggable to this position
+  itemBeingDragged.dragTo(point);
+
+  // If there are any droppable elements at this position, notify them
+  for (var i = 0; i < droppables.length; i++) {
+    if (droppables[i].contains(point) && !droppables[i].isBeingDraggedOver) {
+      droppables[i].onDragEnter();
+    } else if (!droppables[i].contains(point) && droppables[i].isBeingDraggedOver) {
+      droppables[i].onDragExit();
+    }
+  }
+}
+
+function onDocumentMouseUp(mouseUpEvent) {
+  // If any droppable is being dragged over, accept the drop
+  for (var i = 0; i < droppables.length; i++) {
+    if (droppables[i].isBeingDraggedOver) {
+      droppables[i].onDragDrop(itemBeingDragged);
+    }
+  }
+
+  // Reset the position of the item being dragged and clear out the document event handlers
+  itemBeingDragged.reset(mouseUpEvent);
+}
+
+var Draggable = function (elementId) {
+  this.init(elementId);
+};
+
+Draggable.prototype = {
+  init: function (element) {
+    if (typeof element === "string") element = document.getElementById(element);
+
+    this.element = element;
+    if(!this.element.classList.contains('draggable')) {
+      this.element.className += "draggable";
+    }
+
+    var self = this;
+
+    this.element.onmousedown = function (mouseDownEvent) {
+      this.style.zIndex = "1000";
+
+      itemBeingDragged = self;
+
+      mouseDownPoint.x = mouseDownEvent.pageX;
+      mouseDownPoint.y = mouseDownEvent.pageY;
+
+      document.onmousemove = onDocumentMouseMove;
+      document.onmouseup = onDocumentMouseUp;
+    };
+  },
+
+  // Called when the mouse is moved (after having been pressed on this element)
+  dragTo: function (point) {
+    this.element.style.left = (point.x - mouseDownPoint.x) + "px";
+    this.element.style.top = (point.y - mouseDownPoint.y) + "px";
+    this.element.classList.add('moving-element');
+  },
+
+  // Called when the mouse is lifted (after having been pressed on this element)
+  reset: function () {
+    this.element.style.zIndex = "";
+    this.element.style.left = "";
+    this.element.style.top = "";
+    this.element.classList.remove('moving-element');
+
+    itemBeingDragged = null;
+
+    mouseDownPoint.x = 0;
+    mouseDownPoint.y = 0;
+
+    document.onmousemove = null;
+    document.onmouseup = null;
+  }
+};
+
+// customDragDrop is a custom function which will be called when a Draggable is dropped
+// on this Droppable; it is passed the Draggable that was dropped
+var Droppable = function (element, customDragDrop) {
+  this.init(element, customDragDrop);
+};
+
+Droppable.prototype = {
+  init: function (element, customDragDrop) {
+    if (typeof element === "string") element = document.getElementById(element);
+    this.element = element;
+    this.isBeingDraggedOver = false;
+    this.customDragDrop = customDragDrop;
+    droppables.push(this);
+  },
+
+  // Calculate the top-left coordinate of this element
+  position: function () {
+    var position = { x: this.element.offsetLeft, y: this.element.offsetTop };
+    var offsetParent = this.element.offsetParent;
+    while (offsetParent) {
+      position.x += offsetParent.offsetLeft;
+      position.y += offsetParent.offsetTop;
+      offsetParent = offsetParent.offsetParent;
+    }
+    return position;
+  },
+
+  // Calculate whether the given coordinate falls within this element's boundaries
+  contains: function (point) {
+    var topLeft = this.position();
+    var bottomRight = {
+      x: topLeft.x + this.element.offsetWidth,
+      y: topLeft.y + this.element.offsetHeight
+    };
+    return (
+      (topLeft.x < point.x && topLeft.y < point.y)
+      || 
+      (point.x < bottomRight.x && point.y < bottomRight.y)
+    );
+
+  },
+
+  // Called when an item is dragged into this element
+  onDragEnter: function () {
+    this.isBeingDraggedOver = true;
+    this.element.className += "dragOver";
+  },
+
+  // Called when an item is dragged out of this element
+  onDragExit: function () {
+    this.isBeingDraggedOver = false;
+    this.element.className = this.element.className.replace(/\bdragOver\b/, "");
+  },
+
+  // Called when an item is dropped on this element
+  onDragDrop: function (draggable) {
+    this.onDragExit();
+    this.customDragDrop(draggable);
+  }
+};
+
+function touchHandler(event) {
+  var touches = event.changedTouches,
+    first = touches[0],
+    type = "";
+  switch (event.type) {
+    case "touchstart": type = "mousedown"; break;
+    case "touchmove": type = "mousemove"; break;
+    case "touchend": type = "mouseup"; break;
+    default: return;
+  }
+
+  var simulatedEvent = document.createEvent("MouseEvent");
+  simulatedEvent.initMouseEvent(type, true, true, window, 1,
+    first.screenX, first.screenY,
+    first.clientX, first.clientY, false,
+    false, false, false, 0/*left*/, null);
+
+  first.target.dispatchEvent(simulatedEvent);
+  event.preventDefault();
+}
+
+function mapDocumentTouchToMouse() {
+  document.addEventListener("touchstart", touchHandler, true);
+  document.addEventListener("touchmove", touchHandler, true);
+  document.addEventListener("touchend", touchHandler, true);
+  document.addEventListener("touchcancel", touchHandler, true);
+}
+
+mapDocumentTouchToMouse();
+
 window.onload = function () {
   my_list = getOpenItems();
   if (my_list) {
@@ -76,6 +251,7 @@ function render() {
         `;
     document.getElementById('list-item-data').innerHTML += html;
   })
+  initDragdrop();
 }
 
 function renderNewList(item) {
@@ -107,6 +283,7 @@ function renderNewList(item) {
     </div>
   `;
   document.getElementById('list-item-data').innerHTML += html;
+  initDragdrop();
 }
 
 function deleteItem(name) {
@@ -185,4 +362,24 @@ function deleteLi(slug, vl) {
   let slug_item = convertToSlug(vl);
   var li_item = document.querySelector(`li[id=li-${slug}-${slug_item}]`);
   li_item.remove();
+}
+
+function initDragdrop() {
+  my_list.forEach(item => {
+    let slug = convertToSlug(item.name);
+    var ul = document.querySelector(`ul[id=${slug}]`);
+
+    let open_items = item.openItems;
+    open_items.forEach(item => {
+      let slug_item = convertToSlug(item);
+      var liElement = document.querySelector(`li[id=li-${slug}-${slug_item}]`);
+      var liElementDraggable = new Draggable(liElement);
+    })
+
+    var availableMetricsDroppable = new Droppable(ul, function (draggable) {
+      if (this.element !== draggable.element.parentNode) {
+        this.element.appendChild(draggable.element);
+      }
+    });
+  })
 }
